f7e50616a28e8cf685b962ebbdf25bc0383164e5
turner749 <alexander_k_turner@homedepot.com>
2021-08-30 23:32:32 -0400

    initial commit

---
+++ src/components/Header.vue
@@ -0,0 +1,24 @@
+<template>
+  <header>
+    <h1>{{ title }}</h1>
+    <Button text="Add Task1" color="orange"></Button>
+    <Button text="Add Task2"></Button>
+    <Button text="Add Task3"></Button>
+  </header>
+</template>
+
+<script>
+import Button from "./Button";
+
+export default {
+  name: "Header",
+  components: {
+    Button,
+  },
+  props: {
+    title: String,
+  }
+};
+</script>
+
+<style scoped></style>
---
+++ src/views/Home.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="home">
+    <h1>Home Page</h1>
+    <Header title="Task Tracker"></Header>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+
+import Header from "../components/Header";
+
+export default {
+  name: "Home",
+  components: { Header },
+};
+</script>
